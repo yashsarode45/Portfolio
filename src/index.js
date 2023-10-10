a60366e47b9d71c9d066cb1c3a3aff30fc8d7684
Yash Sarode <yashsarode45@gmail.com>
2023-10-10 23:26:39 +0530

    reactGA added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import ReactGA from "react-ga4";
 
+ReactGA.initialize("G-L4T59HDT4G");
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
